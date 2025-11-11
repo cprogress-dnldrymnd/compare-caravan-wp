e5c804188e6e5bde3ee3ac498d315b6b2802a8f4
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-11-11 21:57:44 +0800

    Update single-product.php

--- single-product.php
+++ single-product.php
@@ -119,13 +119,14 @@ if ($seats) {
                                 <?php if ($virtual_tour) { ?>
                                     <div class="tab-pane fade" id="virtual-tour-tab-pane" role="tabpanel" aria-labelledby="virtual-tour-tab" tabindex="0">
                                         <div class="iframe-holder">
-                                            <iframe src="<?= get_youtube_embed_url($virtual_tour) ?> " frameborder="0"></iframe>
+                                            <iframe src="<?= $virtual_tour ?> " frameborder="0"></iframe>
                                         </div>
                                     </div>
                                 <?php } ?>
                                 <?php if ($video) { ?>
                                     <div class="tab-pane fade" id="video-tab-pane" role="tabpanel" aria-labelledby="video-tab" tabindex="0">
-                                        [video section]
+                                        <iframe src="<?= get_youtube_embed_url($virtual_tour) ?> " frameborder="0"></iframe>
+
                                     </div>
                                 <?php } ?>
 
