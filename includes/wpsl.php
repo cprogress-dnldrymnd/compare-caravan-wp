a2cf7a00d634eef2cdb106a5d2570d33ca415670
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-12-03 01:02:41 +0800

    c

--- includes/wpsl.php
+++ includes/wpsl.php
@@ -10,9 +10,9 @@ function custom_wpsl_add_listing_info_field($meta_fields)
 
 
     $meta_fields[__('Listing Information', 'wpsl')] = array(
-        'listing_information' => array(
+        'listing_information_1' => array(
             'label'    => __('Listing Information', 'wpsl'),
-            'type'  => 'textarea', // Defines the input type
+            'type'  => 'text', // Defines the input type
         ),
     );
 
@@ -28,8 +28,8 @@ add_filter('wpsl_frontend_meta_fields', 'custom_wpsl_frontend_listing_info');
 function custom_wpsl_frontend_listing_info($store_fields)
 {
 
-    $store_fields['wpsl_listing_information'] = array(
-        'name' => 'listing_information',
+    $store_fields['wpsl_listing_information_1'] = array(
+        'name' => 'listing_information_1',
     );
 
     return $store_fields;
@@ -81,9 +81,9 @@ function custom_listing_template()
     $listing_template .= "\t\t\t" . '<span  class="contact-details-span"><svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13"> <g id="Icon_feather-globe" data-name="Icon feather-globe" transform="translate(-2.5 -2.5)"> <path id="Path_1331" data-name="Path 1331" d="M15,9A6,6,0,1,1,9,3,6,6,0,0,1,15,9Z" fill="none" stroke="#171717" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/> <path id="Path_1332" data-name="Path 1332" d="M3,18H15" transform="translate(0 -9)" fill="none" stroke="#171717" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/> <path id="Path_1333" data-name="Path 1333" d="M14.4,3a9.18,9.18,0,0,1,2.4,6,9.18,9.18,0,0,1-2.4,6A9.18,9.18,0,0,1,12,9a9.18,9.18,0,0,1,2.4-6Z" transform="translate(-5.4)" fill="none" stroke="#171717" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/> </g> </svg><%= url %></span>' . "\r\n";
     $listing_template .= "\t\t\t" . '<% } %>' . "\r\n";
     $listing_template .= "\t\t\t" . '</div>' . "\r\n";
-    $listing_template .= '<% if ( listing_information ) { %>';
+    $listing_template .= '<% if ( listing_information_1 ) { %>';
     $listing_template .= '<div class="wpsl-listing-info fs-14">' . "\r\n";
-    $listing_template .= '<p><%= listing_information %></p>' . "\r\n";
+    $listing_template .= '<p><%= listing_information_1 %></p>' . "\r\n";
     $listing_template .= '</div>';
     $listing_template .= '<% } %>';
 
