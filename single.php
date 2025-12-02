1aed788c02011dcca26ef24ba0901b552887271d
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-12-02 23:04:04 +0800

    Update single.php

--- single.php
+++ single.php
@@ -25,9 +25,11 @@
                             </li>
                             <li class="d-flex gap-3 align-items-center">
                                 <span class="icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
-                                        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"></path>
-                                    </svg></span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tags" viewBox="0 0 16 16">
+                                        <path d="M3 2v4.586l7 7L14.586 9l-7-7zM2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586z" />
+                                        <path d="M5.5 5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1m0 1a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3M1 7.086a1 1 0 0 0 .293.707L8.75 15.25l-.043.043a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 0 7.586V3a1 1 0 0 1 1-1z" />
+                                    </svg>
+                                </span>
                                 Caravan
                             </li>
                         </ul>
@@ -86,7 +88,7 @@
                                             </div>
                                         </div>
                                         <div class="contact-us">
-                                          <a href="mailto:ste@digitallydisruptive.co.uk" class="btn btn-primary btn-lg">Contact Us</a>
+                                            <a href="mailto:ste@digitallydisruptive.co.uk" class="btn btn-primary btn-lg">Contact Us</a>
                                         </div>
                                     </div>
                                 </div>
