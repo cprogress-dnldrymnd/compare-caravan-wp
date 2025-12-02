8db41db4755fff736cb9c5c518f3505d6888ffc8
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-12-02 23:46:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,32 @@
 <?php get_header() ?>
-<section class="hero background--color position-relative ">
+<section class="hero background--color position-relative <?= $is_range == true ? 'is-range-hero' : '' ?>">
+    <?php
+    if ($is_range == true) {
+        echo '<div class="background-image">';
+        echo wp_get_attachment_image($thumbnail_id, 'large');
+        echo '</div>';
+    }
+    ?>
     <div class="container position-relative">
-        <div class="breadcrumbs fs-16 mt-3">
-            <ul class="list-inline">
-                <li><a href="/">Home</a></li>
-                <li><span>Manufacturer</span></li>
-                <li><span>Bailey</span></li>
-
-            </ul>
-        </div>
+        <?= breadcrumbs() ?>
         <div class="hero-title-image md-margin-top ">
 
             <div class="row">
                 <div class="col-lg-6 md-padding-bottom">
-                    <h1 class="mb-4">Bailey</h1>
+                    <h1 class="mb-4"><?= $name ?></h1>
+                    <?php if ($description) { ?>
+                        <div class="desc mb-4">
+                            <?= wpautop($description) ?>
+                        </div>
+                    <?php } ?>
                 </div>
+                <?php if ($thumbnail_id && $level == 0) { ?>
+                    <div class="col-lg-6">
+                        <div class="image-box">
+                            <?= wp_get_attachment_image($thumbnail_id, 'large') ?>
+                        </div>
+                    </div>
+                <?php } ?>
 
             </div>
         </div>
