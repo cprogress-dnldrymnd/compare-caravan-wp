9826d8637acf06403dbfc5d3fcd1d3268976d8b5
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-12-02 23:55:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 <?php
 if (is_archive()) {
     $title = get_the_archive_title();
+    $description = get_the_archive_description();
 } else {
     $title = 'Blogs';
+    $description = "Whether you're seeking expert advice or just a bit of information, you've come to the right place. We cover all kinds of caravan and motorhome related topics and insights to keep you up to date.";
 }
 ?>
 <section class="hero background--color background-dark-color-2 position-relative ">
@@ -14,6 +16,11 @@ if (is_archive()) {
             <div class="row">
                 <div class="col-lg-6 md-padding-bottom">
                     <h1 class="mb-4"><?= $title ?></h1>
+                    <?php if ($description) { ?>
+                        <div class="desc mb-4">
+                            <?= wpautop($description) ?>
+                        </div>
+                    <?php } ?>
                 </div>
 
             </div>
