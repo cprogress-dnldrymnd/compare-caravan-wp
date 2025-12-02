eb9a93ca375d677d577a86ea8b12f61e3e63fb40
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-12-03 00:57:32 +0800

    Update wpsl.php

--- includes/wpsl.php
+++ includes/wpsl.php
@@ -82,7 +82,7 @@ function custom_listing_template()
     $listing_template .= "\t\t\t" . '<% } %>' . "\r\n";
     $listing_template .= "\t\t\t" . '</div>' . "\r\n";
     $listing_template .= '<% if ( listing_information ) { %>';
-    $listing_template .= '<div class="wpsl-listing-info">' . "\r\n";
+    $listing_template .= '<div class="wpsl-listing-info fs-14">' . "\r\n";
     $listing_template .= '<p><%= listing_information %></p>' . "\r\n";
     $listing_template .= '</div>';
     $listing_template .= '<% } %>';
