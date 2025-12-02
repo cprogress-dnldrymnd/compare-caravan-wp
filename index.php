889d8429d17ffd7643d0ddcd652fde5516dd0b3b
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-12-02 23:45:12 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php get_header() ?>
-<?php
-the_content();
-?>
+<section class="hero background--color position-relative ">
+    <div class="container position-relative">
+        <div class="breadcrumbs fs-16 mt-3">
+            <ul class="list-inline">
+                <li><a href="/">Home</a></li>
+                <li><span>Manufacturer</span></li>
+                <li><span>Bailey</span></li>
+
+            </ul>
+        </div>
+        <div class="hero-title-image md-margin-top ">
+
+            <div class="row">
+                <div class="col-lg-6 md-padding-bottom">
+                    <h1 class="mb-4">Bailey</h1>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
 <?php get_footer() ?>
