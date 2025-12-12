54539e9cf3a38b5dd0e5d5a948aaccf3fbdaaa89
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2025-12-12 21:10:37 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ if (is_archive()) {
     $title = get_the_archive_title();
     $description = get_the_archive_description();
 } else {
-    $title = 'Leisure Vehicle News';
+    $title = 'Latest News';
     $description = "Whether you're seeking expert advice or just a bit of information, you've come to the right place. We cover all kinds of caravan and motorhome related topics and insights to keep you up to date.";
 }
-?>xxx
+?>
 <section class="hero background--color background-dark-color-2 position-relative ">
     <div class="container position-relative">
         <?= breadcrumbs() ?>
