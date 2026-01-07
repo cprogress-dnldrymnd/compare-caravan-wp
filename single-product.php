fb714b4dde39b722edb000824a23ad9b44af1a5c
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-01-07 18:40:18 +0800

    Update single-product.php

--- single-product.php
+++ single-product.php
@@ -462,24 +462,14 @@ if ($seats) {
         </div>
     </div>
 </section>
-<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasEnquireDealer" aria-labelledby="offcanvasEnquireDealerLabel">
+<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasEnquireDealer" aria-labelledby="offcanvasEnquireDealerLabel">
     <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasEnquireDealerLabel">Offcanvas</h5>
+        <h5 class="offcanvas-title" id="offcanvasEnquireDealerLabel">[]</h5>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body">
         <div>
-            Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
-        </div>
-        <div class="dropdown mt-3">
-            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
-                Dropdown button
-            </button>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
+            <?= do_shortcode('[contact-form-7 id="e44c382" title="Contact form 1"]') ?>
         </div>
     </div>
 </div>
