71803fb11efa1a67de7e6e65592a5ec0d9126073
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-01-07 19:47:12 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,6 +24,7 @@ function enquire_dealer() {
 
         jQuery('input[name="dealer-email"]').val(email);
         jQuery('input[name="dealer-name"]').val(dealer_name);
+        jQuery('input[name="vehicle-name"]').val(vechile_name);
         jQuery('#offcanvasEnquireDealerLabel span').html('Enquire about ' + dealer_name);
     });
 }
