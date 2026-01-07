c653a0bdc5c3727e2dcecf85c293fa4589e5caad
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-01-07 19:22:34 +0800

    Update custom-functions.php

--- includes/custom-functions.php
+++ includes/custom-functions.php
@@ -120,9 +120,11 @@ function breadcrumbs()
                     <?php
                     $term_parent = get_term_by('id', $term->parent, $term->taxonomy);
                     ?>
-                    <li><a href="<?=  get_term_link($term_parent->term_id, 'product_brand') ?>"><?= $term_parent->name ?></a></li>
+                    <li><a href="<?= get_term_link($term_parent->term_id, 'product_brand') ?>"><?= $term_parent->name ?></a></li>
+                    <li><span><?= $name ?></span>x1</li>
+                <?php } else { ?>
+                    <li><span><?= $name ?></span></li>
                 <?php } ?>
-                <li><span><?= $name ?></span></li>
             <?php } ?>
 
             <?php if (is_product()) { ?>
