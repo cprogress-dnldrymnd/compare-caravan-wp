ceb99a4524c66352bd235ed14b04e8c3dcfb7294
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-01-21 17:19:22 +0800

    Update single-product.php

--- single-product.php
+++ single-product.php
@@ -469,8 +469,6 @@ if ($seats) {
                                     </div>
 
                                 </div>
-
-
                             </div>
                             <div class="find-dealer">
                                 <div class="heading-icon mb-3">
@@ -482,6 +480,18 @@ if ($seats) {
                                     </h3>
                                 </div>
                                 <?= do_shortcode('[wpsl category="' . get_product_brand_slugs_by_id($product->get_id(), false, true) . '" ]') ?>
+                            </div>
+
+                            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasFindDealer" aria-labelledby="offcanvasFindDealerLabel">
+                                <div class="offcanvas-header">
+                                    <h5 class="offcanvas-title" id="offcanvasFindDealerLabel"><span></span></h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                </div>
+                                <div class="offcanvas-body">
+                                    <div>
+                                        <?= do_shortcode('[contact-form-7 id="e44c382" title="Dealer Enquiry"]') ?>
+                                    </div>
+                                </div>
                             </div>
 
                             <div class="listing-pricing background-text text-white">
@@ -500,7 +510,7 @@ if ($seats) {
                                         </div>
                                     </div>
                                     <div class="col-auto d-block d-lg-none">
-                                        <a href="#" class="btn btn-primary">
+                                        <a href="#" class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasFindDealer">
                                             Find a Dealer
                                         </a>
                                     </div>
